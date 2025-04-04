0bbd1a3fe6f74501407732e4c15f84042bfc6d4e
David <mercadolibre@Macbook-Pro.local>
2025-04-04 17:07:14 -0300

    actualizacion

--- src/assets/components/Body.jsx
+++ src/assets/components/Body.jsx
@@ -1,41 +1,50 @@
 import { Container } from 'react-bootstrap';
+import Carousel  from 'react-bootstrap/Carousel';
 import CardImageSup from './CardImageSup';
 import Presentacion from './Presentacion'
 import '../css/cardimagesup.css';
 
 export default function Body() {
+
+  const images = [
+    { link: 'https://i.ibb.co/5jt3dbj/Recurso-18.png', alt: 'recurso18' },
+    { link: 'https://i.ibb.co/TKqcPD7/Recurso-17.png', alt: 'recurso17' },
+    { link: 'https://i.ibb.co/X2pr44t/Recurso-16.png', alt: 'recurso16' },
+    { link: 'https://i.ibb.co/5jHNyr7/Recurso-15.png', alt: 'recurso15' },
+    { link: 'https://i.ibb.co/V9RXc9J/Recurso-14.png', alt: 'recurso14' },
+  ];
+
   return (
     <>
-      <Container className='sup-image-grid'>
-        <CardImageSup
-          linkImagenCaput='https://i.ibb.co/5jt3dbj/Recurso-18.png'
-          altImageCaput='recurso18'
-        />
-        <CardImageSup
-          linkImagenCaput='https://i.ibb.co/TKqcPD7/Recurso-17.png'
-          altImageCaput='recurso17'
-        />
-        <CardImageSup
-          linkImagenCaput='https://i.ibb.co/X2pr44t/Recurso-16.png'
-          altImageCaput='recurso16'
-        />
-        <CardImageSup
-          linkImagenCaput='https://i.ibb.co/5jHNyr7/Recurso-15.png'
-          altImageCaput='recurso15'
-        />
-        <CardImageSup
-          linkImagenCaput='https://i.ibb.co/V9RXc9J/Recurso-14.png'
-          altImageCaput='recurso14'
-        />
-        <CardImageSup />
-        <CardImageSup />
-        <CardImageSup />
-
+<Container className="sup-image-carousel">
+        <Carousel>
+          {images.map((image, index) => (
+            <Carousel.Item key={index}>
+              <img
+                className="d-block"
+                src={image.link}
+                alt={image.alt}
+              />
+            </Carousel.Item>
+          ))}
+        </Carousel>
       </Container>
+{/* 
+      <Container className='sup-image-grid'>
+        {images.map((image, index) => (
+          <CardImageSup
+            key={index}
+            linkImagenCaput={image.link}
+            altImageCaput={image.alt}
+          />
+        ))}
+        <CardImageSup />
+        <CardImageSup />
+        <CardImageSup />
+      </Container> */}
       <Container>
         <hr />
       </Container>
-
       <Presentacion />
     </>
   );
